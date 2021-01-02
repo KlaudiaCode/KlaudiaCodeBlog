7568fdfe1712ef3a5984a1c67a2e16c3f64f0694
Klaudia Domanska <oposyyy@gmail.com>
2021-01-02 19:47:04 +0100

    Add default scope to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,8 @@
 class Article < ApplicationRecord
   belongs_to :user 
   has_one_attached :image
-
+  
+  default_scope { order(created_at: :desc) }
   scope :has_image, -> {joins(image_attachment: :blob)}
 
   validates :title, presence:true, length: {minimum: 6, maximum: 100}
