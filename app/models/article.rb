82eacd24d27e958fbb143dcbf3ee7dd5f077b77f
Klaudia Domanska <oposyyy@gmail.com>
2021-01-02 15:28:06 +0100

    Add styling for image

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,5 +5,5 @@ class Article < ApplicationRecord
   scope :has_image, -> {joins(image_attachment: :blob)}
 
   validates :title, presence:true, length: {minimum: 6, maximum: 100}
-  validates :description, presence:true, length: {minimum: 10, maximum: 300}
+  validates :description, presence:true, length: {minimum: 10, maximum: 1000}
 end
